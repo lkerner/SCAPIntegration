0f8554f27e35cbbfc8192bc75d43ffddc7505286
Lucy Kerner <lkerner@lkerner.redhat.local>
2017-03-15 17:33:57 -0400

    adding AddCustomAtributes ruby code to SCAP integration

---
+++ AddCustomAttributes.rb
@@ -0,0 +1,17 @@
+#Author: Lucy Kerner(lkerner@redhat.com)
+
+$evm.log(:info, "add_custom_attribute started")
+#
+# Get the VM object
+#
+vm = $evm.root['vm']
+#
+# Get the dialog values
+#
+key   = $evm.root['dialog_key']
+value = $evm.root['dialog_value']
+#
+# Set the custom attribute
+#
+vm.custom_set(key, value)
+exit MIQ_OK
